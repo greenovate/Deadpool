9031508e8e4b41b86aae3c3058edeab8a50dfb44
greenovate <greenovate@users.noreply.github.com>
2026-05-11 18:47:30 -0400

    Bump version to 1.6.1

--- Core.lua
+++ Core.lua
@@ -4,7 +4,7 @@
 ----------------------------------------------------------------------
 
 Deadpool = {}
-Deadpool.version = "1.6.0"
+Deadpool.version = "1.6.1"
 Deadpool.prefix = "DEADPOOL"
 Deadpool.modules = {}
 
